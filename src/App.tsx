bcad8b61a328a7dbfd56c24f9861715f051eb016
Alexey Malyshev <lordniken@mail.ru>
2020-10-12 13:38:21 +0300

    Login page fn name fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,6 @@ import React from 'react'
 import LoginPage from './pages/Login'
 import 'antd/dist/antd.css';
 
-export default ():React.FC => <LoginPage />
+const Login:React.FC = () => <LoginPage />
+
+export default Login
