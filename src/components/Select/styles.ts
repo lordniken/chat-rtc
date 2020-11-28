764e2ccd0e2416e30f11df709af9c727deae3225
Alexey Malyshev <lordniken@mail.ru>
2020-11-28 17:04:39 +0300

    Select styled

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -8,32 +8,13 @@ export const StyledWrapper = styled.div`
 
 export const StyledSelect = styled(Select)`
   width: 240px;
-`;
+  
+  .css-yk16xz-control {
+    border-color: transparent;
+    transition: border-color 0.3s ease-out;
 
-/*
-
-export const StyledSelect = styled.div`
-  display: inline-block;
-  position: relative;
-  padding: 14px 30px 14px 16px;
-  min-width: 240px;
-  border-radius: ${({ theme }) => theme.borderRadius};
-  border: 1px solid transparent;
-  outline: 0;
-  background-color: ${({ theme }) => theme.colors.inputBackground};
-  transition: border-color 0.3s ease-out;
-  user-select: none;
-  cursor: pointer;
-
-  &:after {
-    content: '';
-    position: absolute;
-    background: url(${dropdownIcon}) no-repeat center;
-    width: 10px;
-    height: 7px;
-    right: 10px;
-    top: calc(50% - 3.5px);
+    &:hover {
+      border-color: ${({ theme }) => theme.colors.inputBorder};
+    }
   }
 `;
-
-*/
